4029795b226056339fa5b364b288189f36e9f0ab
Calvin Cieslak <calvin.cieslak@gmail.com>
2020-04-21 15:03:57 -0700

    Core calc functionality

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,42 @@
 struct Beacon {
     x: f32,
     y: f32,
-    distance: f32,
+    d: f32,
 }
 
-fn calc(beacons: [Beacon; 3]) {
-    println!("beacons --- {:#?}", beacons);
+#[derive(Debug)]
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+fn calc(b: [Beacon; 3]) -> Point {
+    let k: f32 = ((b[0].x).powi(2) + (b[0].y).powi(2)
+        - (b[1].x).powi(2)
+        - (b[1].y).powi(2)
+        - (b[0].d).powi(2)
+        + (b[1].d).powi(2))
+        / (2.0 * (b[0].y - b[1].y))
+        - ((b[0].x).powi(2) + (b[0].y).powi(2)
+            - (b[2].x).powi(2)
+            - (b[2].y).powi(2)
+            - (b[0].d).powi(2)
+            + (b[2].d).powi(2))
+            / (2.0 * (b[0].y - b[2].y));
+
+    let j: f32 = ((b[2].x - b[0].x) / (b[0].y - b[2].y)) - ((b[1].x - b[0].x) / (b[0].y - b[1].y));
+
+    let x: f32 = k / j;
+
+    let y: f32 = ((b[1].x - b[0].x) / (b[0].y - b[1].y)) * x
+        + ((b[0].x).powi(2) + (b[0].y).powi(2)
+            - (b[1].x).powi(2)
+            - (b[1].y).powi(2)
+            - (b[0].d).powi(2)
+            + (b[1].d).powi(2))
+            / (2.0 * (b[0].y - b[1].y));
+
+    Point { x, y }
 }
 
 fn main() {
@@ -14,19 +45,20 @@ fn main() {
         Beacon {
             x: 2.0,
             y: 4.0,
-            distance: 5.7,
+            d: 5.7,
         },
         Beacon {
             x: 5.5,
             y: 13.0,
-            distance: 6.8,
+            d: 6.8,
         },
         Beacon {
             x: 11.5,
             y: 2.0,
-            distance: 6.4,
+            d: 6.4,
         },
     ];
 
-    calc(beacons)
+    let point: Point = calc(beacons);
+    println!("point --- {:#?}", point);
 }
